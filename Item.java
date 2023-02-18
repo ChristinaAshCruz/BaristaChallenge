d1ca64fabe61d3da11d4dc03d5ac147c9943439d
ChristinaAshCruz <christina.ash.cruz@gmail.com>
2023-02-18 12:22:56 -0800

    Updated class files + added getters/setters

--- Item.java
+++ Item.java
@@ -1,9 +1,25 @@
 public class Item {
-    public String name;
-    public double price;
+    private String name;
+    private double price;
 
     public Item(String name, double price){
         this.name = name;
         this.price = price;
+    }
+
+    public String getItemName(){
+        return name;
+    }
+
+    public void setItemName(String itemName){
+        name = itemName;
+    }
+
+    public double getItemPrice(){
+        return price;
+    }
+
+    public void setItemPrice(double itemPrice){
+        price = itemPrice;
     }
 }
--- Order.java
+++ Order.java
@@ -1,15 +1,42 @@
 import java.util.ArrayList;
 
 public class Order {
-     
-    public String name;
-    public double total;
-    public boolean ready;
-    public ArrayList<Object> items;
+    private String name;
+    private boolean ready;
+    private ArrayList<Object> items;
 
-    public Order(String name, double total, boolean ready, ArrayList<Object> items){
+    public Order(){
+        // ArrayList<Object> orderItems = new ArrayList<>();
+        this("Guest", false, null);
+    }
+
+    public Order(String name, boolean ready, ArrayList<Object> items){
         this.name = name;
         this.ready = ready;
         this.items = items;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String newName){
+        name = newName;
+    }
+
+    public boolean getOrderStatus(){
+        return ready;
+    }
+
+    public void setOrderStatus(boolean status){
+        ready = status;
+    }
+
+    public ArrayList<Object> getOrder(){
+        return items;
+    }
+
+    public void setOrder(ArrayList<Object> orderItems){
+        items = orderItems;
     }
 }
