984360fd536bdd3cef39f69133cad0d0cb4afb10
ChristinaAshCruz <christina.ash.cruz@gmail.com>
2023-02-18 11:56:07 -0800

    Created Item and Order classes

---
+++ Order.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Order {
+     
+    public String name;
+    public double total;
+    public boolean ready;
+    public ArrayList<Object> items;
+
+    public Order(String name, double total, boolean ready, ArrayList<Object> items){
+        this.name = name;
+        this.ready = ready;
+        this.items = items;
+    }
+}
